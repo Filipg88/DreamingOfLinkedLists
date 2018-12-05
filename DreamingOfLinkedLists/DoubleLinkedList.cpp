5241d0b55dc9ea88e3c638b42eb101119fcb7edf
TheMac <marcus88sjoberg@gmail.com>
2018-12-05 10:47:13 +0100

    added test data

--- DreamingOfLinkedLists/DoubleLinkedList.cpp
+++ DreamingOfLinkedLists/DoubleLinkedList.cpp
@@ -4,6 +4,8 @@
 
 DoubleLinkedList::DoubleLinkedList()
 {
+
+
 }
 
 
--- DreamingOfLinkedLists/DoubleLinkedList.h
+++ DreamingOfLinkedLists/DoubleLinkedList.h
@@ -1,7 +1,10 @@
 #pragma once
 class DoubleLinkedList
 {
+private:
+	int data;
 public:
+
 	DoubleLinkedList();
 	~DoubleLinkedList();
 };
