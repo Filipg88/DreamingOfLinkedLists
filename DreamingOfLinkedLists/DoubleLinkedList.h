93995f7cb9bf9d01dbbd6561534ac98173add5d6
TheMac <marcus88sjoberg@gmail.com>
2018-12-07 09:10:25 +0100

    doublyLinked shell

--- DreamingOfLinkedLists/DoubleLinkedList.h
+++ DreamingOfLinkedLists/DoubleLinkedList.h
@@ -1,11 +1,47 @@
 #pragma once
+#include <iostream>
+
+template <class T>
 class DoubleLinkedList
 {
-private:
 
 public:
+	typedef struct Node
+	{
+		T data;
+		Node* next;
+		Node* prev;
+	}Node;
 
-	DoubleLinkedList();
+	DoubleLinkedList()
+	{
+		this->count = 0;
+		this->head = NULL;
+		this->tail = NULL;
+	}
+
+	int GetCount() {
+		return count;
+	}
+
+	void AddToList(T data) 
+	{
+		Node *newNode = new Node;
+		newNode->data = data;
+		newNode->next = NULL;
+		newNode->prev = NULL;
+		if (GetCount() == 0)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+	}
+
+	
 	~DoubleLinkedList();
+private:
+	int count;
+	Node* head;
+	Node* tail;
 };
 
